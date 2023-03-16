b4b493c7492a8edd980af6e6b786f5075e713d86
Surya Duraivenkatesh <surya.duraivenkatesh@gmail.com>
2023-03-16 01:34:11 -0700

    Add files via upload

---
+++ Mode.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Mode 
+{
+    public static void mode(int[] arr)
+    {
+        HashMap<Integer, Integer> modes = new HashMap<>(); // Hashmap to hold multiple modes in case there is.
+
+        int lastModeFreq = 1, currnum;
+        Arrays.sort(arr, 0, arr.length); // Sorting input array; no return from this function, it modifies the input array directly.
+        for (int i = 1; i < arr.length; i++) // Going through the sorted array.
+        {
+            currnum = arr[i];
+            while (arr[i] == arr[i-1]) // Mode detected.
+            {
+                lastModeFreq++; // Increasing freq.
+                if (i == (arr.length - 1)) // When we are at the last element; edge case.
+                {
+                    break;
+                }
+                i++;// Going to next entry in case this same mode has a higher frequency.
+            }
+            modes.put(currnum, lastModeFreq); // Adding the mode as the key, and the freq as the value.
+            lastModeFreq = 1; // Resetting lastModeFreq for the next possible mode.
+        }
+        int maxValueInMap = (Collections.max(modes.values())); // Finding the highest freq in the hashmap of modes with their respective frequencies.
+        System.out.println("Frequency is: " + maxValueInMap); // Even if there are multiple modes, they will all have the same frequency.
+        System.out.println("Modes are: ");
+        for (Map.Entry<Integer, Integer> entry : modes.entrySet()) // Traversing through hashmap to find which modes have the highest frequency found to display that number.
+        {
+            if (entry.getValue() == maxValueInMap)
+            {
+                System.out.print(entry.getKey() + " "); // Printing the modes with the highest frequency.
+            }
+        }
+        System.out.println(" "); // Note: We go through the hashmap drama mostly since we could have multiple modes with the same (highest) frequency.
+    }
+    public static void main(String args[])
+    {
+        int[] arr = new int[100];
+        Random rand = new Random();
+        for (int i = 0; i < arr.length; i++)
+        {
+            arr[i] = 1 + rand.nextInt(365);
+        }
+        mode(arr); // Passing input array into mode function.
+    }
+}
